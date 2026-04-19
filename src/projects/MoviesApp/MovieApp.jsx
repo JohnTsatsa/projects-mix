e8b00070d0827c6f502106bb323896682c51e608
JohnTsatsa <tsatsanisjohn@gmail.com>
2026-04-20 02:29:39 +0300

    created two  project templates

---
+++ src/projects/MoviesApp/MovieApp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MovieApp() {
+  return (
+    <div>
+      <h1>This is movies app</h1>
+    </div>
+  );
+}
+
+export default MovieApp;
---
+++ src/projects/TasksApp/TasksApp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TasksApp() {
+  return (
+    <div>
+      <h1>This is tasks app</h1>
+    </div>
+  );
+}
+
+export default TasksApp;
